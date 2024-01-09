66e38b5e031b9ce3b7adec1eeb6d400800cc5b9a
Mukul Kumar <mkunboxing@gmail.com>
2024-01-09 21:13:05 +0530

    projects complete

--- src/components/features/Features.js
+++ src/components/features/Features.js
@@ -6,7 +6,7 @@ import { featuresData } from "../../data/data";
 const Features = () => {
   return (
     <section id='features'
-     className='w-full  pb-20  border-b-[1px] border-b-white'>
+     className='w-full  py-20  border-b-[1px] border-b-black'>
         <Title title="Features" des="What I Do" />
 
         <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-20">
